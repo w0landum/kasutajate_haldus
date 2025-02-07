27604758360765fbe6afea59e1c9dbab1bc5a31b
Roland Sinirahu <Roland.sinirahu@voco.ee>
2025-02-06 23:14:45 -0800

    Ülesanne 2

---
+++ lokaalse_kasutaja_kustutamine.ps1
@@ -0,0 +1,15 @@
+﻿$eesnimi = Read-Host "Palun sisesta oma eesnimi "
+$perekonnanimi = Read-Host "Palun sisesta oma perekonnanimi "
+
+$kasutajanimi = $eesnimi.ToLower() + "." + $perekonnanimi.ToLower()
+
+Write-Host "Süsteemist eemldatava kasutajanimi on" $kasutajanimi
+
+$kasutajaeksisteerib = Get-LocalUser $kasutajanimi -ErrorAction SilentlyContinue
+
+if ($kasutajaeksisteerib) {
+    Remove-LocalUser -Name $kasutajanimi -ErrorAction Stop
+    Write-Host "$kasutajanimi on edukalt kustutatud."
+} else {
+    Write-Host "Error: $kasutajanimi ei leitud."
+}
