0d7f1bd8ce381b9c3c6bd7c1e7271e55d7f1ed48
Roland Sinirahu <Roland.sinirahu@voco.ee>
2025-02-10 03:23:58 -0800

    Ülesanne 4

---
+++ AD_kasutaja_kustutamine.ps1
@@ -0,0 +1,14 @@
+﻿$eesnimi = Read-Host "Palun sisesta kasutaja eesnimi"
+$perekonnanimi = Read-Host "Palun sisesta kasutaja perekonnanimi"
+
+$kasutajanimi = $eesnimi.ToLower() + "." + $perekonnanimi.ToLower()
+
+Write-Host "$kasutajanimi läheb süsteemist eemaldamisele."
+
+try {
+Remove-LocalUser -Name $kasutajanimi -Erroraction Stop
+
+Write-Host "Kasutaja $kasutajanimi on edukalt eemaldatud."
+} catch {
+    Write-Host "Kahjuks on tekkinud segadus $kasutajanimi eemaldamisega. Error: $_"
+}
